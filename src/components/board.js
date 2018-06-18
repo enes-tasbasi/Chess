187f63242bcd99f66cf8a19ae92cac93658116d5
Enes Tasbasi <enestasbasi@outlook.com>
2018-06-17 23:38:42 -0400

    store the positions in state

--- src/components/board.js
+++ src/components/board.js
@@ -1,91 +1,142 @@
 import React from 'react';
-//import {ReactDOM} from 'react-dom';
 
-let Board = () => (
-    <div className="container">
-        <table>
-            <tr>
-            <th className="square"><i className="fas fa-chess-rook" /></th>
-            <th className="square"><i className="fas fa-chess-bishop"/></th>
-            <th className="square"><i className="fas fa-chess-knight"/></th>
-            <th className="square"><i className="fas fa-chess-queen"/></th>
-            <th className="square"><i className="fas fa-chess-king"/></th>
-            <th className="square"><i className="fas fa-chess-knight"/></th>
-            <th className="square"><i className="fas fa-chess-bishop"/></th>
-            <th className="square"><i className="fas fa-chess-rook"/></th>
-        </tr>
-            <tr>
-                <th className="square"><i className="fas fa-chess-pawn" /></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-            </tr>
-            <tr>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-            </tr>
-            <tr>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-            </tr>
-            <tr>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-            </tr>
-            <tr>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-                <th className="square"></th>
-            </tr>
-            <tr>
-                <th className="square"><i className="fas fa-chess-pawn" /></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-                <th className="square"><i className="fas fa-chess-pawn"/></th>
-            </tr>
-            <tr>
-                <th className="square"><i style={{color: 'white'}} className="fas fa-chess-rook" /></th>
-                <th className="square"><i className="fas fa-chess-bishop"/></th>
-                <th className="square"><i className="fas fa-chess-knight"/></th>
-                <th className="square"><i className="fas fa-chess-queen"/></th>
-                <th className="square"><i className="fas fa-chess-king"/></th>
-                <th className="square"><i className="fas fa-chess-knight"/></th>
-                <th className="square"><i className="fas fa-chess-bishop"/></th>
-                <th className="square"><i className="fas fa-chess-rook"/></th>
-            </tr>
-        </table>
-    </div>
-);
+import Icon from './chessIcons';
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            board: {
+                rank8: {
+                    1: "blackRook",
+                    2: "blackBishop",
+                    3: "blackKnight",
+                    4: "blackQueen",
+                    5: "blackKing",
+                    6: "blackKnight",
+                    7: "blackBishop",
+                    8: "blackRook",
+                },
+                rank7: {
+                    1: "blackPawn",
+                    2: "blackPawn",
+                    3: "blackPawn",
+                    4: "blackPawn",
+                    5: "blackPawn",
+                    6: "blackPawn",
+                    7: "blackPawn",
+                    8: "blackPawn",
+                },
+                rank6: {
+                    1: "",
+                    2: "",
+                    3: "",
+                    4: "",
+                    5: "",
+                    6: "",
+                    7: "",
+                    8: "",
+                },
+                rank5: {
+                    1: "",
+                    2: "",
+                    3: "",
+                    4: "",
+                    5: "",
+                    6: "",
+                    7: "",
+                    8: "",
+                },
+                rank4: {
+                    1: "",
+                    2: "",
+                    3: "",
+                    4: "",
+                    5: "",
+                    6: "",
+                    7: "",
+                    8: "",
+                },
+                rank3: {
+                    1: "",
+                    2: "",
+                    3: "",
+                    4: "",
+                    5: "",
+                    6: "",
+                    7: "",
+                    8: "",
+                },
+                rank2: {
+                    1: "whitePawn",
+                    2: "whitePawn",
+                    3: "whitePawn",
+                    4: "whitePawn",
+                    5: "whitePawn",
+                    6: "whitePawn",
+                    7: "whitePawn",
+                    8: "whitePawn",
+                },
+                rank1: {
+                    1: "whiteRook",
+                    2: "whiteBishop",
+                    3: "whiteKnight",
+                    4: "whiteQueen",
+                    5: "whiteKing",
+                    6: "whiteKnight",
+                    7: "whiteBishop",
+                    8: "whiteRook",
+                }
+            }
+            // board : {
+            // a1: "whiteRook",
+            // a2: "whiteBishop",
+            // a3: "whiteKnight",
+            // a4: "whiteQueen",
+            // a5: "whiteKing",
+            // a6: "whiteKnight",
+            // a7: "whiteBishop",
+            // a8: "whiteRook",
+            // b1: "whitePawn",
+            // b2: "whitePawn",
+            // b3: "whitePawn",
+            // b4: "whitePawn",
+            // b5: "whitePawn",
+            // b6: "whitePawn",
+            // b7: "whitePawn",
+            // b8: "whitePawn",
+            // }
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <table>
+
+                    {Object.keys(this.state.board).map((key) => {
+
+                        let eachRank = this.state.board[key];
+
+                        return (
+                            <tr>
+                                <th className="square"><Icon iconName={eachRank["1"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["2"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["3"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["4"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["5"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["6"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["7"]}/></th>
+                                <th className="square"><Icon iconName={eachRank["8"]}/></th>
+                            </tr>
+                        )
+                    })}
+
+                </table>
+            </div>
+        )
+    }
+}
 
 export default Board;
---
+++ src/components/chessIcons.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+
+class IconMaker extends React.Component {
+    handleClick() {
+        console.log('test...');
+    }
+    render() {
+        return (
+            <i  style={{color: this.props.color}} className={this.props.className} onClick={this.handleClick} />
+        )
+    }
+    }
+
+class Icon extends React.Component {
+
+    blackRook() {
+        return <IconMaker color="black" className="fas fa-chess-rook"/>
+    }
+
+    blackBishop() {
+        return <IconMaker color="black" className="fas fa-chess-bishop"/>
+    }
+
+    blackKnight() {
+        return <i style={{color: "black"}} className="fas fa-chess-knight"/>
+    }
+
+    blackQueen() {
+        return <i style={{color: "black"}} className="fas fa-chess-queen"/>
+    }
+
+    blackKing() {
+        return <i style={{color: "black"}} className="fas fa-chess-king"/>
+    }
+
+    blackPawn() {
+        return <i style={{color: "black"}} className="fas fa-chess-pawn"/>
+    }
+
+    whiteRook() {
+        return <i style={{color: "white"}} className="fas fa-chess-rook"/>
+    }
+
+    whiteBishop() {
+        return <i style={{color: "white"}} className="fas fa-chess-bishop"/>
+    }
+
+    whiteKnight() {
+        return <i style={{color: "white"}} className="fas fa-chess-knight"/>
+    }
+
+    whiteQueen() {
+        return <i style={{color: "white"}} className="fas fa-chess-queen"/>
+    }
+
+    whiteKing() {
+        return <i style={{color: "white"}} className="fas fa-chess-king"/>
+    }
+
+    whitePawn() {
+        return <i style={{color: "white"}} className="fas fa-chess-pawn"/>
+    }
+
+    render() {
+        switch (this.props.iconName) {
+            case "blackRook":
+                return this.blackRook();
+            case "blackBishop":
+                return this.blackBishop();
+            case "blackKnight":
+                return this.blackKnight();
+            case "blackQueen":
+                return this.blackQueen();
+            case "blackKing":
+                return this.blackKing();
+            case "blackPawn":
+                return this.blackPawn();
+            case "whiteRook":
+                return this.whiteRook();
+            case "whiteBishop":
+                return this.whiteBishop();
+            case "whiteKnight":
+                return this.whiteKnight();
+            case "whiteQueen":
+                return this.whiteQueen();
+            case "whiteKing":
+                return this.whiteKing();
+            case "whitePawn":
+                return this.whitePawn();
+            default:
+                return ""
+        }
+    }
+}
+
+
+export default Icon;
